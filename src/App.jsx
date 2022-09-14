294c3520b300dfd0147ec47f5c2939fbaae8099d
Việt An <vietan00892b@gmail.com>
2022-09-14 18:27:46 +0700

    Incorporated Frontend Mentor's boilerplate files
    - Changed ESLint, Vite configs
    - Renamed `main.jsx` to `index.jsx`
    - Deleted CSS files

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,18 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <div className="attribution">
+        Challenge by
+        {' '}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>
+        .
+        Coded by
+        {' '}
+        <a href="https://github.com/vietan0" target="_blank" rel="noreferrer">Việt An</a>
+        .
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
